ff5185cd0375a73dba402c16be586b56a30146f1
rozay13 <161240000531@unisnu.ac.id>
2022-06-07 00:15:14 +0700

    ketujuh

---
+++ index.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title>Jati RM</title>
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <!--enable mobile device-->
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <!--fontawesome css-->
+ <link rel="stylesheet" href="css/font-awesome.min.css">
+ <!--bootstrap css-->
+ <link rel="stylesheet" href="css/bootstrap.min.css">
+ <!--animate css-->
+ <link rel="stylesheet" href="css/animate-wow.css">
+ <!--main css-->
+ <link rel="stylesheet" href="css/style.css">
+ <link rel="stylesheet" href="css/bootstrap-select.min.css">
+ <link rel="stylesheet" href="css/slick.min.css">
+ <!--responsive css-->
+ <link rel="stylesheet" href="css/responsive.css">
+ <link rel="stylesheet" href="css/main.css">
+
+</head>
+<a id="button"></a>
+<body>
+
+ 
+
+ <header id="header" class="top-head">
+  <!-- Static navbar -->
+  <nav class="navbar navbar-default">
+   <div class="container-fluid">
+    <div class="row">
+     <div class="col-md-4 col-sm-12 left-rs">
+      <div class="navbar-header">
+       <button type="button" id="top-menu" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false"> 
+        <span class="sr-only">Toggle navigation</span> 
+        <span class="icon-bar"></span> 
+        <span class="icon-bar"></span> 
+        <span class="icon-bar"></span> 
+       </button>
+       <a href="index.php" class="navbar-brand"><img src="images/logo/logo.png" alt="" /></a>
+      </div>
+
+     </div>
+     <div class="col-md-8 col-sm-12">
+      <div class="navbar-left"> 
+       <div class="nav-b hidden-xs">
+        <div class="nav-box">
+         <ul>
+          <li><a href="index.php">Beranda</a></li>
+          <li><a href="profile.php">Kontak kami</a></li>
+          <li><a href="about.php">Tentang Kami</a></li>
+         </ul>
+        </div>
+       </div>
+      </div>
+     </div>
+    </div>
+   </div>
+   <!--/.container-fluid --> 
+  </nav>
+ </header>
+ <!-- Modal -->
+ <div class="modal fade lug" id="myModal" role="dialog">
+  <div class="modal-dialog">
+   <!-- Modal content-->
+   <div class="modal-content">
+    <div class="modal-header">
+     <button type="button" class="close" data-dismiss="modal">&times;</button>
+     <h4 class="modal-title">Change</h4>
+    </div>
+
+   </div>
+  </div>
+ </div>
+ <div id="sidebar" class="top-nav">
+  <ul id="sidebar-nav" class="sidebar-nav">
+   <li><a href="index.php">Beranda</a></li>
+   <li><a href="profile.php">Kontak kami</a></li>
+   <li><a href="about.php">Tentang Kami</a></li>
+  </ul>
+ </div>
+ <div class="page-content-product">
+  <div class="main-product">
+   <div class="container">
+    <div class="row clearfix">
+     <div class="find-box" style="margin-top: -50px; margin-bottom: -40px;">
+      <h4>Jati RM<br>Produsen Furniture dan Interior dari Jepara
+       <br>dengan Bahan Kayu Jati Terbaik</h4>
+       <h4><br>Kategori Jati RM</h4>
+
+      </div>
+     </div>
+     <div class="row clearfix">
+      <div class="col-lg-3 col-sm-6 col-md-3">
+       <a href="productpage.html">
+        <div class="box-img">
+         <h4>Kursi</h4>
+         <img src="images/product/kursi.jpg" alt="" style="border-radius: 100px; height: 150px;" />
+        </div>
+       </a>
+      </div>
+                  <!-- <div class="col-lg-3 col-sm-6 col-md-3">
+                     <a href="productpage.html">
+                        <div class="box-img">
+                           <h4>Product</h4>
+                           <img src="images/product/2.png" alt="" />
+                        </div>
+                     </a>
+                    </div> -->
+                 <!--  <div class="col-lg-3 col-sm-6 col-md-3">
+                     <a href="productpage.html">
+                        <div class="box-img">
+                           <h4>Product</h4>
+                           <img src="images/product/4.png" alt="" />
+                        </div>
+                     </a>
+                    </div> -->
+                  <!-- <div class="col-lg-3 col-sm-6 col-md-3">
+                     <a href="productpage.html">
+                        <div class="box-img">
+                           <h4>Product</h4>
+                           <img src="images/product/5.png" alt="" />
+                        </div>
+                     </a>
+                    </div> -->
+                    <div class="col-lg-3 col-sm-6 col-md-3">
+                     <a href="productpage.html">
+                      <div class="box-img">
+                       <h4>Lemari</h4>
+                       <img src="images/product/almari.png" alt="" style="border-radius: 100px; height: 150px;"/>
+                      </div>
+                     </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 col-md-3">
+                     <a href="productpage.html">
+                      <div class="box-img">
+                       <h4>Gazebo</h4>
+                       <img src="images/product/gazebo.png" alt="" style="border-radius: 100px; height: 150px;"/>
+                      </div>
+                     </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 col-md-3">
+                     <a href="productpage.html">
+                      <div class="box-img">
+                       <h4>Gebyok</h4>
+                       <img src="images/product/gebyok.png" alt="" style="border-radius: 100px; height: 150px;"/>
+                      </div>
+                     </a>
+                    </div>
+                     <div class="col-lg-3 col-sm-6 col-md-3">
+                     <a href="productpage.html">
+                      <div class="box-img">
+                       <h4>Tempat Tidur</h4>
+                       <img src="images/product/depan.png" alt="" style="border-radius: 100px; height: 150px;"/>
+                      </div>
+                     </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 col-md-3">
+                     <a href="productpage.html">
+                      <div class="box-img">
+                       <h4>Meja Rias</h4>
+                       <img src="images/product/mejarias.png" alt="" style="border-radius: 100px; height: 150px;"/>
+                      </div>
+                     </a>
+                    </div>
+
+                   </div>
+                  </div>
+                 </div>
+                </div>
+               
+
+              <div class="row">
+               <div class="main-start-box">
+                <div class="bg_img_left"><img src="images/bg_img1.png" alt="#" /></div>
+                <div class="container">
+                 <div class="buyer-box clearfix">
+                  <div class="col-md-6 col-sm-6 wow fadeIn" data-wow-delay="0.1s">
+                   <div class="left-buyer">
+                    <img class="img-responsive" src="images/product/kursi.jpg" alt="#" />
+                   </div>
+                  </div>
+                  <div class="col-md-6 col-sm-6 wow fadeIn" data-wow-delay="0.4s">
+                   <div class="right-buyer">
+                    <h2>Kursi Tamu Mewah<br>
+                     <span>dengan bahan kayu jati berkualitas</span>
+                    </h2>
+                    <p>Ruang tamu yang ideal adalah ruangan yang bisa memberikan kenyamanan bagi tamu yang datang ke rumah Anda. Selain kenyamanan, keindahan ruang tamu juga menjadi penilaian tersendiri bagi para tamu. Salah satu cara mempercantik ruang tamu adalah meletakkkan kursi tamu jati mewah di ruang tamu anda. Selain tahan lama, kayu jati juga memiliki kesan mewah dan elegan untuk melengkapi ruangan di tempat anda.
+                     <br>Sebelum memutuskan untuk membeli kursi tamu dari jati, ukiurlah ruangan anda terlebih dahulu. Jangan sampai kursi tamu yang di beli terlalu kecil atau terlalu besar berada diruangan anda. Jika memungkinkan, pastikan ruang tamu Anda bisa digunakan lalu lalang. Jika ruang tamu Anda kecil, maka anda bisa menggunakan kursi tamu jati dengan gaya klasik dan minimalis yang ukurannya sesuai dengan ruang tamu Anda.
+                    </p>
+                    <a href="#">Selengkapnya</a> <a href="https://wa.me/628568735182?text=Hallo, Saya ingin menanyakan tentang produk Anda-----------JATI RM-----------">Live Chat</a>
+                   </div>
+                  </div>
+                 </div>
+                </div>
+               </div>
+              </div>
+              <div class="row">
+               <div class="bg_img_right"><img src="images/bg_img1.png" alt="#" /></div>
+               <div class="main-start-box">
+                <div class="container">
+                 <div class="supplier clearfix">
+                  <div class="col-md-5 col-sm-6">
+                   <div class="left-supplier">
+                    <h4>Kithcen Set</h4>
+                    <h2>Nikmati keuntungannya ! <br><span>Menggunakan Kitchen Set di Dapur Anda</span></h2>
+                    <p>Dapur merupakan salah satu area di rumah dengan fungsi yang sangat vital. Saat mendesainnya, anda harus memikirkan secara cermat agar dapur Anda bisa menjadi tempat yang nyaman untuk berkegiatan.
+                      <br>Selain desain dapur secara keseluruhan, pemilihan dekorasi, dan penyimpana di dapur juga mendapat peran yang besar untuk membuat dapur Anda terlihat cantik dan menyenangkan.
+                      <br> Kitchen set merupakan perabotan yang biasa terpasang di dalam ruangan dapur yang memiliki keuntungan antara lain membuat dapur lebih rapi, Tidak bingung menyimpan peralatan dapur, Bisa digunakan menyimpan makanan, Memberikan kemudahan dalam menyimpan bumbu dapur, serta Mempercantik tampilan dapur.
+                      <br>Oleh sebab itu kami menyediakan berbagai macam kitchen set mulai dari model dan ukuran yang bisa di sesuaikan dengan kriteria tempat Anda.
+                    </p>
+                    .
+                    <a href="#">Create a supplier account</a>
+                   </div>
+                  </div>
+                  <div class="col-md-7 col-sm-6">
+                   <div class="right-supplier">
+                    <img class="img-responsive" src="images/product/dapur2.jpg" alt="#" />
+                   </div>
+                  </div>
+                 </div>
+                </div>
+               </div>
+              </div>
+             </div>
+            </div>
+            <div class="products">
+             <div class="main-products">
+              <h2>TRENDING PRODUCTS ON JATI RM</h2>
+              <div class="product-slidr">
+               <div class="slider">
+                <div>
+                 <div class="prod-box">
+                  <div class="prod-i">
+                   <img src="images/tr1.png" alt="#" />
+                  </div>
+                  <div class="prod-dit clearfix">
+                   <div class="dit-t clearfix">
+                    <div class="left-ti">
+                     <h4>Table with Lights</h4>
+                     <p>By <span>Beko</span> under <span>Lights</span></p>
+                    </div>
+
+                   </div>
+
+                  </div>
+                 </div>
+                </div>
+                <div>
+                 <div class="prod-box">
+                  <div class="prod-i">
+                   <img src="images/tr2.png" alt="#" />
+                  </div>
+                  <div class="prod-dit clearfix">
+                   <div class="dit-t clearfix">
+                    <div class="left-ti">
+                     <h4>Treehouse Bed</h4>
+                     <p>By <span>Beko</span> under <span>Lights</span></p>
+                    </div>
+
+                   </div>
+
+                  </div>
+                 </div>
+                </div>
+                <div>
+                 <div class="prod-box">
+                  <div class="prod-i">
+                   <img src="images/tr3.png" alt="#" />
+                  </div>
+                  <div class="prod-dit clearfix">
+                   <div class="dit-t clearfix">
+                    <div class="left-ti">
+                     <h4>Wood Sofaplatform</h4>
+                     <p>By <span>Beko</span> under <span>Lights</span></p>
+                    </div>
+                   </div>
+                  </div>
+                 </div>
+                </div>
+                <div>
+                 <div class="prod-box">
+                  <div class="prod-i">
+                   <img src="images/tr4.png" alt="#" />
+                  </div>
+                  <div class="prod-dit clearfix">
+                   <div class="dit-t clearfix">
+                    <div class="left-ti">
+                     <h4>Wall Sticker</h4>
+                     <p>By <span>Beko</span> under <span>Lights</span></p>
+                    </div>
+                   </div>
+                  </div>
+                 </div>
+                </div>
+                <div>
+                 <div class="prod-box">
+                  <div class="prod-i">
+                   <img src="images/tr1.png" alt="#" />
+                  </div>
+                  <div class="prod-dit clearfix">
+                   <div class="dit-t clearfix">
+                    <div class="left-ti">
+                     <h4>Treehouse Bed</h4>
+                     <p>By <span>Beko</span> under <span>Lights</span></p>
+                    </div>
+                   </div>
+                  </div>
+                 </div>
+                </div>
+               </div>
+              </div>
+             </div>
+            </div>
+
+            <?php include 'footer.php'; ?>
+
+<!-- Back to top button -->
+ <button id="back-to-top-btn"><i class="fa fa-angle-double-up"></i>
+    
+  </button>
+            <!--main js--> 
+            <script src="js/jquery-1.12.4.min.js"></script> 
+            <!--bootstrap js--> 
+            <script src="js/bootstrap.min.js"></script> 
+            <script src="js/bootstrap-select.min.js"></script>
+            <script src="js/slick.min.js"></script> 
+            <script src="js/wow.min.js"></script>
+            <!--custom js--> 
+            <script src="js/custom.js"></script>
+            <script src="js/main.js"></script>
+            
+            <!--  <?php include 'top.php'; ?>  -->
+
+           </body>
+           </html>
